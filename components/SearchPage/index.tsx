699ba5ac9e0df27fd52ac215021265a7a59b7d90
Denys Lapin <denys.lapin@auto1.com>
2023-04-04 22:25:20 +0300

    Setup template

---
+++ components/SearchPage/index.tsx
@@ -0,0 +1,12 @@
+import { Col, Container, Row } from "react-bootstrap";
+import styles from './styles.module.css';
+
+export const SearchPage = () => {
+  return (
+    <Container className={styles.container} fluid>
+      <Row>
+        <Col>Blabla</Col>
+      </Row>
+    </Container>
+  );
+};
